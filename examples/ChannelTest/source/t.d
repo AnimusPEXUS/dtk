043162a2df448f3dcc9b1aebe901bf2bc3df1631
Animus PEXUS <animuspexus@protonmail.com>
2022-07-02 17:02:51 +0300

    Channel works

--- examples/ChannelTest/source/t.d
+++ examples/ChannelTest/source/t.d
@@ -9,7 +9,7 @@ import dtk.miscs.Channel;
 // more documentation
 // https://tour.dlang.org/tour/en/multithreading/synchronization-sharing
 
-const THREADS_NUMBER_TO_TEST = 10;
+const THREADS_NUMBER_TO_TEST = 50;
 
 void prt(string s)
 {
--- source/dtk/miscs/Channel.d
+++ source/dtk/miscs/Channel.d
@@ -25,11 +25,11 @@ class Channel(T)
     {
         T[] pool;
         Mutex pool_mx;
-        // Mutex pull_mx;
-        // Mutex push_mx;
+        Mutex push_mx;
+        Mutex pull_mx;
 
-        shared Condition cond;
-        shared Mutex cond_mx;
+        Condition cond;
+        Mutex cond_mx;
 
         bool waiting;
 
@@ -38,57 +38,65 @@ class Channel(T)
 
     this()
     {
-        cond_mx = new shared Mutex();
-        // cond_mx.lock();
-        cond = new shared Condition(cond_mx);
+        cond_mx = new Mutex();
+        cond = new Condition(cond_mx);
 
         pool_mx = new Mutex();
+        push_mx = new Mutex();
+        pull_mx = new Mutex();
     }
 
-    synchronized void push(T value)
+    void push(T value) shared
     {
-        prt("push --> (start)");
-        scope (exit) prt("push <-- (exit)");
-        synchronized (pool_mx)
+        synchronized (push_mx)
         {
-            pool ~= value;
-            prt("   cond.notify()");
-            cond.notify();
+            prt("push --> (start)");
+            scope (exit) prt("push <-- (exit)");
+            synchronized (pool_mx)
+            {
+                pool ~= value;
+                prt("   cond.notify()");
+                cond.notify();
+            }
         }
     }
 
-    synchronized T pull()
+    T pull() shared
     {
-        prt("pull --> (start %d)".format(pull_counter));
-        scope (exit)
+        synchronized (pull_mx)
         {
-            prt("pull <-- (exit %d)".format(pull_counter));
-            atomicOp!"+="(this.pull_counter, 1);
+            {
+                prt("pull <-- (exit %d)".format(pull_counter));
+                atomicOp!"+="(this.pull_counter, 1);
+            }
+            T ret;
+
+            while (pool.length == 0)
+            {
+                prt("   cond.wait()");
+
+                synchronized(cond_mx)
+                {
+                    cond.wait();
+                }
+
+                prt("   cond.wait() exited");
+            }
+
+            synchronized (pool_mx)
+            {
+                auto pl = pool.length;
+
+                ret = pool[0];
+
+                if (pl == 1)
+                    pool = pool[0 .. 0];
+                else
+                    pool = pool[1 .. $];
+            }
+
+            return ret;
         }
-
-        T ret;
-
-        while (pool.length == 0)
-        {
-            //cond_mx.lock();
-            prt("   cond.wait()");
-            cond.wait();
-            prt("   cond.wait() exited");
-        }
-
-        synchronized (pool_mx)
-        {
-            auto pl = pool.length;
-
-            ret = pool[0];
-
-            if (pl == 1)
-                pool = pool[0 .. 0];
-            else
-                pool = pool[1 .. $];
-        }
-
-        return ret;
     }
 }
 
