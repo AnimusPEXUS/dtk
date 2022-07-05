ef3d497ef156e5019885e5329216d2e63c5aaf62
Animus PEXUS <animuspexus@protonmail.com>
2022-07-06 02:47:06 +0300

    do autoformatting on generated files

--- generators/KeyboardSourcesGenerator/run_sdl.sh
+++ generators/KeyboardSourcesGenerator/run_sdl.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 ./generate_sdl_keyconversion.d
+dfmt keyconversion.d > keyconversion.d2
+mv keyconversion.d2 keyconversion.d
--- generators/KeyboardSourcesGenerator/run_types.sh
+++ generators/KeyboardSourcesGenerator/run_types.sh
@@ -2,3 +2,11 @@
 
 ./generate_types_EnumKeyboardKeyCode.d
 ./generate_types_EnumKeyboardModCode.d
+
+dfmt EnumKeyboardKeyCode.d > EnumKeyboardKeyCode.d2
+mv EnumKeyboardKeyCode.d2 EnumKeyboardKeyCode.d
+
+dfmt EnumKeyboardModCode.d > EnumKeyboardModCode.d2
+mv EnumKeyboardModCode.d2 EnumKeyboardModCode.d
+
+
