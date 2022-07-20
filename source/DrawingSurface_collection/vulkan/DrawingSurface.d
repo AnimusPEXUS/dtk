2307e75bdc9d28ad3bdaf4cc3a0221919715502a
Animus PEXUS <animuspexus@protonmail.com>
2022-07-20 07:34:12 +0300

    starting Vulkan DrawingSurface

---
+++ source/DrawingSurface_collection/vulkan/DrawingSurface.d
@@ -0,0 +1,22 @@
+module dtk.DrawingSurface_collection.vulkan.DrawingSurface;
+
+import std.stdio;
+import std.typecons;
+import std.math;
+import std.conv;
+
+import dtk.types.Position2D;
+import dtk.types.Size2D;
+import dtk.types.Color;
+import dtk.types.LineStyle;
+import dtk.types.FillStyle;
+import dtk.types.fontinfo;
+import dtk.types.TextStyle;
+import dtk.types.Image;
+
+import dtk.interfaces.DrawingSurfaceI;
+import dtk.interfaces.FaceI;
+
+class DrawingSurface : DrawingSurfaceI
+{
+}
